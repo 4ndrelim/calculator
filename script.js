c3e1f77d77f1880431e2f0054e5529c509d0059c
andre <tiltedaf3@gmail.com>
2022-06-18 05:52:26 +0800

    Complete access buttons and function to determine which operator to use

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 /**
+ * State variables
+ * Numbers represented as string to avoid integer overflow
+ */
+let operandOne = '';
+let operandTwo = '';
+let appliedOperaton = null;
+let toReset = false;
+
+
+/**
+ * Access buttons
+ */
+const allNumbers = document.querySelector('.numbers');
+const allOperations = document.querySelector('.operations');
+const screenView = document.querySelector('#screen');
+const equalsBtn = document.querySelector('#equal');
+const clearBtn = document.querySelector('#clear');
+const allClearBtn = document.querySelector('#all-clear');
+const toggleSignBtn = document.querySelector('#toggle-sign');
+const decimalBtn = document.querySelector('#dot');
+
+
+/**
+ * Event listeners
+ */
+
+
+/**
  * Operations
  */
 
@@ -15,6 +43,9 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        return null;
+    }
     return a / b;
 }
 
@@ -23,6 +54,10 @@ function power(a, b) {
     if (b === 0) {
         return ret;
     }
+    if (a === 0) {
+        return 0;
+    }
+
     if (b > 0) {
         for (let i = 0; i < b; i++) {
             ret *= a;
@@ -33,4 +68,23 @@ function power(a, b) {
         }
     }
     return ret;
+}
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '×':
+            return multiply(a, b);
+        case '÷':
+            return divide(a, b);
+        case '𝑥^':
+            return power(a, b);
+        default:
+            return null;
+    }
 }
